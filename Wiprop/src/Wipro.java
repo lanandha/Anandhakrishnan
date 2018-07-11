faa16a5f4ade6fdc2bdeed6b65e1678744fe914b
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-11 15:24:09 +0530

    Add files via upload

---
+++ Wiprop/src/Wipro.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Wipro {
+public static void main(String[]args){
+	//String s="Wipro";
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	char ch=s.charAt(0);
+	char cc=s.charAt(1);
+	for(int i=0;i<5;i++){
+	System.out.print(ch+""+cc);
+}}
+}
