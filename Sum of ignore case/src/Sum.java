e91d827917f1e51bcf26a9b4891bbcf035ec5df8
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-11 15:39:32 +0530

    Add files via upload

---
+++ Sum of ignore case/src/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[]args){
+		int i;
+		System.out.println("Enter the integer");
+		Scanner s=new Scanner(System.in);
+		
+		
+		
+	}
+
+}
