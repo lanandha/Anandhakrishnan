b460e88b9d67217f0216bef53b67462bf0f02f17
lanandha <41110722+lanandha@users.noreply.github.com>
2018-07-11 15:25:55 +0530

    Add files via upload

---
+++ Volume/src/Volume.java
@@ -0,0 +1,16 @@
+
+public class Volume {
+	public static void main(String args[]) {
+}
+
+   // System.out.println("Test #1 : General case to remove number  22");
+    int[] input = {42, 322, 22, 11, 22, 33, 16};
+    //System.out.println("input : " + Arrays.toString(input) + ", remove 22");
+    int[] output = remove(input, 22);
+	private int[] remove(int[] input2, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	 // removes number 22 from array
+  //  System.out.println("output : " + Arrays.toString(output));
+}}
+ 
